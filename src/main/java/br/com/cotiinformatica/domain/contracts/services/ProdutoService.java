179ea8c3d920d24eb4d57581ccf3a46e0a574b56
Paulo Lima <paulo22tr22@gmail.com>
2024-11-30 11:22:08 -0300

    colocandos os metodos na camada de dominio services

--- src/main/java/br/com/cotiinformatica/domain/contracts/services/ProdutoService.java
+++ src/main/java/br/com/cotiinformatica/domain/contracts/services/ProdutoService.java
@@ -1,5 +1,23 @@
 package br.com.cotiinformatica.domain.contracts.services;
 
-public class ProdutoService {
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.domain.Pageable;
+
+import br.com.cotiinformatica.domain.models.dtos.ProdutoRequest;
+import br.com.cotiinformatica.domain.models.dtos.ProdutoResponse;
+
+public interface ProdutoService {
+
+	ProdutoResponse criarProduto(ProdutoRequest request);
+
+	ProdutoResponse alterarProduto(UUID id, ProdutoRequest request);
+
+	ProdutoResponse inativarProduto(UUID id);
+
+	List<ProdutoResponse> filtrarProdutos(String nome, Pageable pageable);
+
+	ProdutoResponse obterProduto(UUID id);
 
 }
